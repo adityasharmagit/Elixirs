059b9075f072595d3dcb9161ee7388efcc5fa268
Aditya Sharma <as0257233@gmail.com>
2025-11-30 14:49:28 +0530

    change the spline viewer

--- frontend/src/components/design/SplineViewer.jsx
+++ frontend/src/components/design/SplineViewer.jsx
@@ -4,14 +4,14 @@ const SplineViewer = () => {
     useEffect(() => {
         const script = document.createElement("script");
         script.type = "module";
-        script.src = "https://unpkg.com/@splinetool/viewer@1.10.48/build/spline-viewer.js";
+        script.src = "https://unpkg.com/@splinetool/viewer@1.12.5/build/spline-viewer.js";
         document.head.appendChild(script);
     }, []);
 
     return (
         <div className="w-full h-[800px] mt-10 spline-container">
             <spline-viewer
-                url="https://prod.spline.design/T6uk8jlISYwO4NYu/scene.splinecode"
+                url="https://prod.spline.design/BUA9zKNAwitI2jtV/scene.splinecode"
                 loading="lazy"
                 style={{ width: "100%", height: "100%"}}
                 render-frames="20"
